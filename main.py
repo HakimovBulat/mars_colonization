abafb9c4d2656a4ea17ca0bc65e1cd5d5c747e1a
Hakimov Bulat <bulathakimov1704@gmail.com>
2023-02-19 12:10:30 +0300

    Изображение Марса

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 
 app = Flask(__name__)
@@ -21,6 +21,12 @@ def promotion():
                       "Так давайте начнём с Марса!", "Присоединяйся!"
                       ]
     return '<br>'.join(promotion_list)
+
+
+@app.route('/image_mars')
+def image():
+    return f'''<title>Привет, Марс!</title> <h1>Жди нас, Марс!</h1> <img src="{url_for('static', filename='img/mars.jpg')}" 
+           alt="здесь должна была быть картинка, но не нашлась"> <p>Вот он - красивый марс<p>'''
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
